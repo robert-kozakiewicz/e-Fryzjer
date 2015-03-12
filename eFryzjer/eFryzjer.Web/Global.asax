ff8f98942f2822d7aea4b25415b7cd3ed761b6be
Robert <kozakiewicz.robert@gmail.com>
2015-03-12 18:14:42 +0100

    hggfhfgh

---
+++ eFryzjer/eFryzjer.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eFryzjer.Web.MvcApplication" Language="C#" %>
---
+++ eFryzjer/eFryzjer.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eFryzjer.Web.Startup))]
+namespace eFryzjer.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
